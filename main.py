472edd4798ef8e046481fc46cd768d60dd9f906d
Aadhithya K Praveen <78066049+AKP-shadow@users.noreply.github.com>
2022-10-10 23:48:09 +0530

    'updated'

--- main.py
+++ main.py
@@ -4,47 +4,49 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
-def gen(code, country) :
+def gen(code,country) :
     id = Image.open("template.png") #id card template
     country = country.upper()
+    try:
+        path = "C:\\Users\\aadhi\\OneDrive\\Desktop\\idcardgen\\idcardgen\\flags\\" #location of the data such as images to be inserted in the id
+        path += code
+        path += ".png"
+        flag  = Image.open(path)
 
-    path = "C:\\Users\\thean\\Downloads\\MUN\\ID\\flags\\" #location of the data such as images to be inserted in the id
-    path += code
-    path += ".png"
-    flag  = Image.open(path)
+        #positioning of the image on the id based on template dimensions
+        basewidth = 984
+        wpercent = (basewidth/float(flag.size[0]))
+        hsize = int((float(flag.size[1])*float(wpercent)))
+        flag = flag.resize((basewidth,hsize), Image.Resampling.LANCZOS)
 
-    #positioning of the image on the id based on template dimensions
-    basewidth = 984
-    wpercent = (basewidth/float(flag.size[0]))
-    hsize = int((float(flag.size[1])*float(wpercent)))
-    flag = flag.resize((basewidth,hsize), Image.ANTIALIAS)
+        #pasting the image on the template
+        temp=Image.open("template.png")
+        temp.paste(flag, (0,(1329-hsize)))
 
-    #pasting the image on the template
-    temp=Image.open("temp.png")
-    temp.paste(flag, (0,(1329-hsize)))
+        #pasting the text on the id along with scaling font size
+        temp.paste(id, (0,0),id)
+        if (len(country)<=11) :
+            font = ImageFont.truetype("arial.ttf", 100, encoding="unic")
+        elif (len(country)>11 and len(country)<=17):
+            font = ImageFont.truetype("arial.ttf", 70, encoding="unic")
+        elif(len(country)>17 and len(country)<=25) :
+            font = ImageFont.truetype("arial.ttf", 55, encoding="unic")
+        else :
+            font = ImageFont.truetype("arial.ttf", 40, encoding="unic")
 
-    #pasting the text on the id along with scaling font size
-    temp.paste(id, (0,0),id)
-    if (len(country)<=11) :
-        font = ImageFont.truetype("Metropolis-Black.ttf", 100, encoding="unic")
-    elif (len(country)>11 and len(country)<=17):
-        font = ImageFont.truetype("Metropolis-Black.ttf", 70, encoding="unic")
-    elif(len(country)>17 and len(country)<=25) :
-        font = ImageFont.truetype("Metropolis-Black.ttf", 55, encoding="unic")
-    else :
-        font = ImageFont.truetype("Metropolis-Black.ttf", 40, encoding="unic")
+        draw = ImageDraw.Draw(temp)
+        w, h = draw.textsize(country, font=font)
+        draw.text(((984-w)/2, 1329-((1329-h)/5)), country, 'white', font)
 
-    draw = ImageDraw.Draw(temp)
-    w, h = draw.textsize(country, font=font)
-    draw.text(((984-w)/2, 1329-((1329-h)/5)), country, 'white', font)
-
-    #saving the id
-    svtext = "disec_"
-    svtext += country
-    svtext += ".png"
-    temp.save(svtext)
-    #temp.convert("CMYK").save("testcmyk.jpeg")
-    print("done")
+        #saving the id
+        svtext = "./ids/disec_"
+        svtext += country
+        svtext += ".png"
+        temp.save(svtext)
+        #temp.convert("CMYK").save("testcmyk.jpeg")
+        print("done")
+    except:
+        pass
 
 
 #reads superset file for id and name
@@ -56,16 +58,13 @@ sheet = 'UNSC'
 file = 'matrix.xlsx'
 
 df = read_excel(file,sheet)
-
-cnt = set(df["Country"]).intersection(set(flags["Country"]))
-
-#calls the function to gnerate
-merg = pd.merge(df, flags, on='Country')
+ 
 i = 0
 j = 0
-while (i<len(merg)) :
-    gen(merg.loc[i,"Code"],merg.loc[i,"Country"])
+# print(df)
+while (i<len(df)) :
+    gen(flags.loc[i,"Code"],flags.loc[i,"Country"])
     i = i+1
 
-#log file for all ids created
-merg.to_csv("disec_done.csv")
+# #log file for all ids created
+# df.to_csv("./ids/disec_done.csv")
