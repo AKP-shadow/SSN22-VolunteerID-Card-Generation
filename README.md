05ca6700003ae802a0148a49066393f0f740b955
Aadhithya K Praveen <78066049+AKP-shadow@users.noreply.github.com>
2022-10-15 19:00:41 +0530

    version_2

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # idcardgen
 A script that auto generates ID cards with various pictures and form fields given the initial template.
-Made it for the SSNMUN19' to generate various ID Cards for the Members
+
 
 
 flagdata.csv contains the mapping of names with images
-matrix.xlsx contain the necessary id's that have to be generated
-template.png is the base ID template file
-flags-ultra.zip contains all the images 
-output.rar contains the id cards that were generated
+ 
+template.jpeg is the base ID template file
+ 
 
+Generated IDs are stored in 'ids' folder.
+
+Update the student_data.csv file and add corresponding pictures in student_pics folder
--- main.py
+++ main.py
@@ -4,79 +4,95 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont,ImageFilter
 
-def rounded_image(img):
-      
+
+def add_corners(img, code):
     height,width = img.size
     lum_img = Image.new('L', [height,width] , 0)
     
     draw = ImageDraw.Draw(lum_img)
     draw.pieslice([(0,0), (height,width)], 0, 360, 
                 fill = 255, outline = "white")
-    img.putalpha(lum_img)
-    im_a_blur = img.filter(ImageFilter.GaussianBlur(4))
     img_arr =np.array(img)
     lum_img_arr =np.array(lum_img)
-    Image.fromarray(lum_img_arr)
     final_img_arr = np.dstack((img_arr,lum_img_arr))
-    # return Image.fromarray(final_img_arr)
-    return im_a_blur
+    Image.fromarray(final_img_arr).save('./temp/'+str(code)+'_test.png')
+    
 
-
+   
+ 
 def gen(code,name,designation) :
-    id = Image.open("Volunteer.png") #id card template
+    id = Image.open("template.jpeg") #id card template
     name = name.upper()
-    try:
-        path = "student_pics/" #location of the data such as images to be inserted in the id (BASED ON REGISTER NO)
-        path += str(code)
-        path += ".jpg"
-        student_pic  = Image.open(path)
+    # try:
+    path = "student_pics/" #location of the data such as images to be inserted in the id (BASED ON REGISTER NO)
+    path += str(code)
+    path += ".jpg"
+    student_pic  = Image.open(path)
 
-        #positioning of the image on the id based on template dimensions
-        basewidth = id.size[0]-600
-        wpercent = (basewidth/float(student_pic.size[0]))
+    #positioning of the image on the id based on template dimensions
+    basewidth = id.size[0]//4
+    wpercent = (basewidth/float(student_pic.size[0]))
+    
+    hsize = int((float(student_pic.size[1])*float(wpercent)))+200
+    print("hsize:",hsize,"wpercent:",wpercent)
+    image_alignment=(170,200)                                                      #IMAGE RESIZE ------------>CHANGE HERE!!
+    # student_pic = student_pic.resize((basewidth,hsize), Image.Resampling.LANCZOS)
+    
+    
+    student_pic = student_pic.resize(image_alignment, Image.Resampling.LANCZOS)
+    add_corners(student_pic,code)
+    student_pic_round = Image.open('./temp/'+str(code)+'_test.png')
+    # student_pic_round.show()
+    #Pasting student image onto the TEMPLATE
+    temp=Image.open("template.jpeg")                                                #TEMPLATE-----------------> CHANGE HERE!!
+    print(temp.size[0]//3,temp.size[0]//3)
+    temp.paste(student_pic_round, (int(temp.size[0]*0.32) ,int(temp.size[1]*0.3)))        #IMAGE ALIGNMENT----------> CHANGE HERE!!
+    
+    
+    
+
+    #SCALING OF FONT_NAME                                                               
+    if (len(name)<=11) :
+        font_name = ImageFont.truetype("arial.ttf", 30, encoding="unic")
+    elif (len(name)>11 and len(name)<=17):
+        font_name = ImageFont.truetype("arial.ttf", 25, encoding="unic")
+    elif(len(name)>17 and len(name)<=25) :
+        font_name = ImageFont.truetype("arial.ttf", 20, encoding="unic")
+    else :
+        font_name = ImageFont.truetype("arial.ttf", 10, encoding="unic")
         
-        hsize = int((float(student_pic.size[1])*float(wpercent)))+200
-        print("hsize:",hsize,"wpercent:",wpercent)
-        image_alignment=(822,1132)                                                      #IMAGE RESIZE ------------>CHANGE HERE!!
-        # flag = flag.resize((basewidth,hsize), Image.Resampling.LANCZOS)
-        student_pic = student_pic.resize(image_alignment, Image.Resampling.LANCZOS)
-
-        #Pasting student image onto the TEMPLATE
-        temp=Image.open("Volunteer.png")                                                #TEMPLATE-----------------> CHANGE HERE!!
-        # student_pic = rounded_image(student_pic)                                      #ROUNDED IMAGE 
-        temp.paste(student_pic, (449,929))                                                     #IMAGE ALIGNMENT----------> CHANGE HERE!!
-
-        #SCALING OF FONT_NAME                                                               
-        if (len(name)<=11) :
-            font_name = ImageFont.truetype("arial.ttf", 150, encoding="unic")
-        elif (len(name)>11 and len(name)<=17):
-            font_name = ImageFont.truetype("arial.ttf", 100, encoding="unic")
-        elif(len(name)>17 and len(name)<=25) :
-            font_name = ImageFont.truetype("arial.ttf", 90, encoding="unic")
-        else :
-            font_name = ImageFont.truetype("arial.ttf", 80, encoding="unic")
-            
-        if (len(designation)<=11) :                                                     #DESIGNATION FONT---------> CHANGE HERE!!
-            font_designation = ImageFont.truetype("arial.ttf", 150, encoding="unic")
-        elif (len(designation)>11 and len(designation)<=17):
-            font_designation = ImageFont.truetype("arial.ttf", 100, encoding="unic")
-        elif(len(designation)>17 and len(designation)<=25) :
-            font_designation = ImageFont.truetype("arial.ttf", 90, encoding="unic")
-        else :
-            font_designation = ImageFont.truetype("arial.ttf", 80, encoding="unic")
-        draw = ImageDraw.Draw(temp)
-        w, h = draw.textsize(name, font=font_name)
-        draw.text(((temp.size[0]-w)/2, temp.size[1]-((temp.size[1]-h)/4)), name, 'black', font_name)                            #NAME TEXT INSERTED HERE!!!
-        draw.text(((temp.size[0]-w)/2, temp.size[1]-((temp.size[1]-h)/4)+200), designation, 'black', font=font_designation )    #DESIGNATION TEXT INSERTED HERE!!
-        draw.text((temp.size[0]//7,369),'SSN INVENTE 6.0','black',font=ImageFont.truetype("arial.ttf", 150, encoding="unic"))   #INVENTE TITLE INSERTED HERE!!
-        #saving the id
-        svtext = "./ids/"
-        svtext += str(code)+'_'+str(designation)
-        svtext += ".png"
-        temp.save(svtext)
-        print("done")
-    except:
-        pass
+        
+        
+        
+    if (len(designation)<=11) :                                                     #DESIGNATION FONT---------> CHANGE HERE!!
+        font_designation = ImageFont.truetype("arial.ttf", 20, encoding="unic")
+    elif (len(designation)>11 and len(designation)<=17):
+        font_designation = ImageFont.truetype("arial.ttf", 18, encoding="unic")
+    elif(len(designation)>17 and len(designation)<=25) :
+        font_designation = ImageFont.truetype("arial.ttf", 15, encoding="unic")
+    else :
+        font_designation = ImageFont.truetype("arial.ttf", 12, encoding="unic")
+        
+        
+        
+        
+        
+    draw = ImageDraw.Draw(temp)
+    w, h = draw.textsize(name, font=font_name)
+    print(w)
+    draw.text(((temp.size[0]-w)/2, temp.size[1]-((temp.size[1]-h)/4)-20), name, 'black', font_name)                            #NAME TEXT INSERTED HERE!!!
+    draw.text(((temp.size[0]- len(designation)*12)//2, temp.size[1]-((temp.size[1]-h)/4)+20), designation.upper(), 'black', font=font_designation )    #DESIGNATION TEXT INSERTED HERE!!
+   
+   
+   
+    #saving the id
+    svtext = "./ids/"
+    svtext += str(code)+'_'+str(designation)
+    svtext += ".png"
+    temp.save(svtext)
+    print("done")
+    # except:
+    #     pass
 
 
 #reads superset file for id and name
