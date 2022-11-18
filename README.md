5ce2e2194650c68586fa322bccdedf62e5fd0868
Aadhithya K Praveen <78066049+AKP-shadow@users.noreply.github.com>
2022-11-18 23:49:02 +0530

    /Read from excel/rmsIDAlignment/generation

--- README.md
+++ README.md
@@ -13,13 +13,11 @@ Update the student_data.csv file and add corresponding pictures in student_pics 
 
 
 INPUT                                                    
-
-![Angelina Jolie_Organiser](https://user-images.githubusercontent.com/78066049/200387879-89b99f72-c73f-4a1e-9e89-7db119d242bf.jpg)
-
+ 
 
 
 
 OUTPUT ID GENERATED
 
 
-![Angelina Jolie_Organiser_Organiser](https://user-images.githubusercontent.com/78066049/200388006-bf9b8437-9a30-408a-8405-4f46ed7d2966.png)
+ 
--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 
-from PIL import Image,ImageDraw,ImageFont,ImageOps
-from cairosvg import svg2png
-from io import BytesIO
+from PIL import Image,ImageDraw,ImageFont
+import numpy as np
 import os
- 
+import pandas as pd
+import shutil
 TEMPLATE_SIZE = (453,615)                               #TEMPLATE RESIZE ------------>CHANGE HERE!!
-FONT_MAIN = "./fonts/arial.ttf"                         #FONT STYLE  ------------>CHANGE HERE!!
+FONT_MAIN = "./fonts/Minion-Bold.otf"                         #FONT STYLE  ------------>CHANGE HERE!!
 
 
 
-NAME_FONT_SIZE = 35
+NAME_FONT_SIZE = 100
+PROF_FONT_SIZE = 9*NAME_FONT_SIZE/24
+
 NAME_ADJUSTMENT_PADDING_X_AXIS = 0
 NAME_ADJUSTMENT_PADDING_Y_AXIS = -20
+PROF_TEXT_COLOR = '#2c2d2d'
 NAME_TEXT_COLOR = 'black'
 
-DESIGNATION_FONT_SIZE = 30
+INSTITUTION_NAME_SIZE = NAME_FONT_SIZE*18/24
 DESIGNATION_ADJUSTMENT_PADDING_X_AXIS = 10
 DESIGNATION_ADJUSTMENT_PADDING_Y_AXIS = 20
-DESIGNATION_TEXT_COLOR = 'black'
+DESIGNATION_TEXT_COLOR = '#155e8b'
 
 
 STUDENT_PIC_BORDER_COLOR = "#1d6dad"
@@ -27,129 +30,109 @@ STUDENT_PIC_SIZE = (170,200)                                  #IMAGE RESIZE ----
 
 
 # Get the list of all files and directories
-def get_file_names(path):
-    dir_list = os.listdir(path)
-    return dir_list
+def CreateDirectory(path):
+    absolute_path = os.path.dirname(__file__)
+    relative_path = f"ids/{path}"
+    full_path = os.path.join(absolute_path, relative_path)
+    if not os.path.exists(full_path):
+        os.makedirs(full_path)
+    else:
+        shutil.rmtree(full_path)           # Removes all the subdirectories!
+        os.makedirs(full_path)
 
 
 
 
-def frame(im, border_color,thickness=5):
-    # Get input image width and height, and calculate output width and height
-    iw, ih = im.size
-    ow, oh = iw+2*thickness, ih+2*thickness
-    # Draw outer black rounded rect into memory as PNG
-    outer = f'<svg width="{ow}" height="{oh}" style="background-color:none"><rect rx="20" ry="20" width="{ow}" height="{oh}" fill="{border_color}"/></svg>'
-    png   = svg2png(bytestring=outer)
-    outer = Image.open(BytesIO(png))
-
-    # Draw inner white rounded rect, offset by thickness into memory as PNG
-    inner = f'<svg width="{ow}" height="{oh}"><rect x="{thickness}" y="{thickness}" rx="20" ry="20" width="{iw}" height="{ih}" fill="white"/></svg>'
-    png   = svg2png(bytestring=inner)
-    inner = Image.open(BytesIO(png)).convert('L')
-
-    # Expand original canvas with black to match output size
-    expanded = ImageOps.expand(im, border=thickness, fill=(29, 109, 173)).convert('RGB')
-
-    # Paste expanded image onto outer black border using inner white rectangle as mask
-    outer.paste(expanded, None, inner)
-    return outer
  
-def gen(name,designation,format) :
-    id = Image.open("template.jpeg") #id card template
-    # name = name.upper()
-    # try:
-    path = "./student_pics/" #location of the data such as images to be inserted in the id (BASED ON REGISTER NO)
-    path += str(name+'_'+designation)
-    path += format
-    # print(path)
-    student_pic  = Image.open(path)
-
-    #positioning of the image on the id based on template dimensions
-    basewidth = id.size[0]//4
-    wpercent = (basewidth/float(student_pic.size[0]))
-    
-    hsize = int((float(student_pic.size[1])*float(wpercent)))+200
-    # print("hsize:",hsize,"wpercent:",wpercent)
-    # student_pic = student_pic.resize((basewidth,hsize), Image.Resampling.LANCZOS)
-    
-    
-    student_pic = student_pic.resize(STUDENT_PIC_SIZE, Image.Resampling.LANCZOS)
-    student_pic_background = frame(student_pic,border_color=STUDENT_PIC_BORDER_COLOR)
-    # rectangle(student_pic)
+def gen(name,institution_name,path) :
+    institution_name = institution_name.strip('*')
+ 
     #Pasting student image onto the TEMPLATE
-    temp=Image.open("template.jpeg")                                                #TEMPLATE-----------------> CHANGE HERE!!
-    temp = temp.resize(TEMPLATE_SIZE)
-    
-    # temp.paste(student_pic, (int(temp.size[0]*0.315) ,int(temp.size[1]*0.3)))        #IMAGE ALIGNMENT----------> CHANGE HERE!!
-    temp.paste(student_pic_background, (int(temp.size[0]*STUDENT_PIC_ADJUSTMENT_X_AXIS) ,int(temp.size[1]*STUDENT_PIC_ADJUSTMENT_Y_AXIS)),student_pic_background)        #IMAGE ALIGNMENT----------> CHANGE HERE!!
-    # print(int(temp.size[0]-STUDENT_PIC_SIZE[0]//2))
+    temp=Image.open("template.png")                                                #TEMPLATE-----------------> CHANGE HERE!!
+    print(temp.size)
+    # temp = temp.resize(TEMPLATE_SIZE)
+ 
  
     t = NAME_FONT_SIZE
-    if (len(name)<=11) :
+    font_name = ImageFont.truetype(FONT_MAIN, int(NAME_FONT_SIZE), encoding="unic")
+    p = PROF_FONT_SIZE
+    font_prof =  ImageFont.truetype(FONT_MAIN, int(PROF_FONT_SIZE), encoding="unic")
+    if (len(name)<=16) :
         font_name = ImageFont.truetype(FONT_MAIN, int(NAME_FONT_SIZE), encoding="unic")
-    elif (len(name)>11 and len(name)<=17):
+    elif (len(name)>16 and len(name)<=25):
         font_name = ImageFont.truetype(FONT_MAIN, int(NAME_FONT_SIZE*0.9), encoding="unic")
-        t = int(NAME_FONT_SIZE*0.9)
-    elif(len(name)>17 and len(name)<=25) :
-        font_name = ImageFont.truetype(FONT_MAIN, int(NAME_FONT_SIZE*0.75), encoding="unic")
-        t  = int(NAME_FONT_SIZE*0.75)
+        t = int(NAME_FONT_SIZE*0.7)
     else :
         font_name = ImageFont.truetype(FONT_MAIN, int(NAME_FONT_SIZE*0.6), encoding="unic")
         t = int(NAME_FONT_SIZE*0.6)
         
         
         
-    s=DESIGNATION_FONT_SIZE
-    if (len(designation)<=11) :                                                     #DESIGNATION FONT---------> CHANGE HERE!!
-        font_designation = ImageFont.truetype(FONT_MAIN, int(DESIGNATION_FONT_SIZE), encoding="unic")
-    elif (len(designation)>11 and len(designation)<=17):
-        font_designation = ImageFont.truetype(FONT_MAIN, int(DESIGNATION_FONT_SIZE*0.8), encoding="unic")
-        s = int(DESIGNATION_FONT_SIZE*0.8)
-    elif(len(designation)>17 and len(designation)<=25) :
-        font_designation = ImageFont.truetype(FONT_MAIN, int(DESIGNATION_FONT_SIZE*0.67), encoding="unic")
-        s=int(DESIGNATION_FONT_SIZE*0.67)
-    else :
-        font_designation = ImageFont.truetype(FONT_MAIN, int(DESIGNATION_FONT_SIZE*0.5), encoding="unic")
-        s = int(DESIGNATION_FONT_SIZE*0.5)
+    s=INSTITUTION_NAME_SIZE
+    font_institution = ImageFont.truetype(FONT_MAIN, int(INSTITUTION_NAME_SIZE), encoding="unic", )
+    if (len(institution_name)<=16) :                                                     #DESIGNATION FONT---------> CHANGE HERE!!
+        font_institution = ImageFont.truetype(FONT_MAIN, int(INSTITUTION_NAME_SIZE), encoding="unic")
+    elif (len(institution_name)>16 and len(institution_name)<=25):
+        font_institution = ImageFont.truetype(FONT_MAIN, int(INSTITUTION_NAME_SIZE*0.8), encoding="unic")
+        s = int(INSTITUTION_NAME_SIZE*0.8)
+    elif(len(institution_name)>25 and len(institution_name)<=50) :
+        font_institution = ImageFont.truetype(FONT_MAIN, int(INSTITUTION_NAME_SIZE*0.67), encoding="unic")
+        s=int(INSTITUTION_NAME_SIZE*0.67)
+ 
         
         
     
             
-    print(s)
     draw = ImageDraw.Draw(temp)
-    w, h = draw.textsize(name, font=font_name)
-    draw.text(((temp.size[0]-w)/2 + NAME_ADJUSTMENT_PADDING_X_AXIS, temp.size[1]-((temp.size[1]-h)/4)+NAME_ADJUSTMENT_PADDING_Y_AXIS), name, NAME_TEXT_COLOR, font_name)                            #NAME TEXT INSERTED HERE!!!
-    desig_adjust_old = len(designation)*DESIGNATION_ADJUSTMENT_PADDING_X_AXIS * (0.05*s)
-    if len(designation)>10:
-        desig_adjust_old = len(designation) * DESIGNATION_ADJUSTMENT_PADDING_X_AXIS * ( 0.046*s)
-    if len(designation)>15:
-        desig_adjust_old = len(designation) * DESIGNATION_ADJUSTMENT_PADDING_X_AXIS * (0.049*s)
-    if len(designation)>=20:
-        desig_adjust_old = len(designation) * DESIGNATION_ADJUSTMENT_PADDING_X_AXIS * (0.047*s)
-    if len(designation)>=25:
-        desig_adjust_old = len(designation) * DESIGNATION_ADJUSTMENT_PADDING_X_AXIS * (0.052*s)
- 
-    draw.text(((temp.size[0]- desig_adjust_old)//2, temp.size[1]-((temp.size[1]-h)/4)+DESIGNATION_ADJUSTMENT_PADDING_Y_AXIS), designation, DESIGNATION_TEXT_COLOR, font=font_designation )    #DESIGNATION TEXT INSERTED HERE!!
+    l,k = draw.textsize(institution_name, font=font_institution)
+    name = name.split('.')
+    print(l,k)
+    print(name)
+    if len(name)>1:
+        w, h = draw.textsize("".join(name[1:]), font=font_name)
+        a, b = draw.textsize(name[0], font=font_name)
+        filename = "".join(name[1:])
+        
+        draw.text((((temp.size[0]-w)/2-a/5) ,2*temp.size[1]/3-h/10), name[0]+'. ', PROF_TEXT_COLOR, font_prof)                            #NAME TEXT INSERTED HERE!!!
+    # draw.text(((temp.size[0]-w)/2 + NAME_ADJUSTMENT_PADDING_X_AXIS, temp.size[1]-((temp.size[1]-h)/3)+NAME_ADJUSTMENT_PADDING_Y_AXIS), name[1], NAME_TEXT_COLOR, font_name,stroke_width=2)                            #NAME TEXT INSERTED HERE!!!
+        draw.text((((temp.size[0]-w)/2+a/5), 0.6367*temp.size[1]), "".join(name[1:]), NAME_TEXT_COLOR, font_name,stroke_width=1)                            #NAME TEXT INSERTED HERE!!!
+    else:
+        w, h = draw.textsize(name[0], font=font_name)
+        draw.text((((temp.size[0]-w)/2), 0.6367*temp.size[1]), name[0], NAME_TEXT_COLOR, font_name,stroke_width=1)                            #NAME TEXT INSERTED HERE!!!
+        filename = name[0]
+        
+    draw.text((((temp.size[0]-l)/2+0.08*l),  2.1*temp.size[1]/3), institution_name, DESIGNATION_TEXT_COLOR, font=font_institution )    #DESIGNATION TEXT INSERTED HERE!!
    
     
-    temp.resize((453,615),Image.Resampling.LANCZOS)
+    # temp.resize((453,615),Image.Resampling.LANCZOS)
     #saving the id
-    svtext = "./ids/"
-    svtext += str(name+'_'+designation)+'_'+str(designation)
-    svtext += ".png"
+    svtext = f"./ids/{path}/"+str(filename.strip()+'_'+institution_name)+".png"
     temp.save(svtext)
     print("done")
     # except:
     #     pass
 
 
-#main file
-for i in get_file_names('.\student_pics'):
-    name = i.split('_')[0]
-    designation = i.split('_')[1][:-4]
-    format = i.split('_')[1][-4:]
-    if format=='jpeg':
-        format= format
-    gen(name,designation,format)
+ 
+xl = pd.ExcelFile('Order of the Talk - RMS Speakers.xlsx')
 
+
+
+# xl.parse(sheet_name)  # read a specific sheet to DataFrame
+for sheet_no in range(len( pd.read_excel('Order of the Talk - RMS Speakers.xlsx',sheet_name=None))-1):
+    CreateDirectory(xl.sheet_names[sheet_no])
+    df = pd.read_excel('Order of the Talk - RMS Speakers.xlsx',sheet_name=sheet_no)
+    start=0
+    
+    for i in df.iloc[:,0]:
+        if i == "S. No. ":
+            break
+        
+        start+=1
+    start+=1
+    for speaker in range(len(df.index)-start):
+        if not  df.iloc[:,0].isnull()[start+speaker] and not df.iloc[start+speaker,0]=="S. No. ":
+            gen(df.iloc[start+speaker,1],df.iloc[start+speaker,2],xl.sheet_names[sheet_no])
+            # print(df.iloc[start+speaker,1])
+            # print(df.iloc[start+speaker,2])
+    
